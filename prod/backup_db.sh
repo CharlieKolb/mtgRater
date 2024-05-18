1b59c8bbc9faefc6deb3fe1585e2177ae35447ad
Charlie Kolb <charlie.kolb@outlook.de>
2024-05-18 18:11:40 +0200

    softcode postgres docker id in backup script

--- prod/backup_db.sh
+++ prod/backup_db.sh
@@ -3,5 +3,5 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>backup_log.out 2>&1
 
-POSTGRES_DOCKER_ID=fd7ebcbdadbf
+POSTGRES_DOCKER_ID=$(sudo docker container ls  | grep 'postgres' | awk '{print $1}')
 sudo docker exec -it $POSTGRES_DOCKER_ID sh -c "pg_dump postgres" | aws s3 cp - s3://mtgraterdbbackups/db_backup_$(date +"%FT%H%M").sql
