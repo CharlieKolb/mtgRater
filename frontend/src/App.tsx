4abb7a6daf82bfb7a635281d33e2f5f81b64ec93
Charlie Kolb <charlie.kolb@outlook.de>
2024-05-22 09:01:19 +0200

    fix font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,10 +12,6 @@ import theme from './theme'
 import { CssBaseline } from '@mui/material/';
 
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import Backend, { Ratings, Collections, CollectionInfo } from './server/backend';
 import CollectionExportButton from './rater/collectionExportButton';
 import CollectionFilterToggles, { configToId, filterCollectionInfo, FilterConfig } from './collectionFilterToggles';
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+// this is needed during dev only
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
